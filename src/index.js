aad76f502fba29f02a967cdaf399adafbc0801d6
nesliprm <nesliprm@gmail.com>
2023-12-11 18:59:18 +0100

    added more instructions to context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateCocktail(event) {
   let apiKey = "040oaf210f6td0bbf39be33790d96a71";
   let prompt = `Display a cocktail recipe that has ${userInput.value} in it.`;
   let context =
-    "Please give clear instructions and provide measurements both in oz and ml. Format your answer in HTML with cocktail name in <h4>, ingredients in <ul> with list-style-type: none, under title 'ingredients', and method in <ul> with list-style-type: decimal under title 'method'. Add the type of glassware to serve at the end, under title 'glassware'. Please add <br> when necessary";
+    "Please give clear instructions and provide measurements both in oz and ml. Format your answer in HTML with cocktail name in <h4>, ingredients in <ul> with list-style-type: none, under title 'ingredients', and method in <ul> with list-style-type: decimal under title 'method'. Add the type of glassware to serve at the end, under title 'glassware'. Please add <br> when necessary.";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
