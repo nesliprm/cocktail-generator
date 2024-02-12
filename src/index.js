f24a63efc2481e09ac81ad35da52e8c02cab3195
nesliprm <nesliprm@gmail.com>
2024-02-12 18:41:09 +0100

    added IBA certified into the prompt

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateCocktail(event) {
 
   let userInput = document.querySelector("#cocktail-input");
   let apiKey = "040oaf210f6td0bbf39be33790d96a71";
-  let prompt = `Display a cocktail recipe that has ${userInput.value} in it.`;
+  let prompt = `Display a IBA-certified cocktail recipe that has ${userInput.value} in it.`;
   let context =
     "Please give clear instructions and provide measurements both in oz and ml. List ALL the necessary ingredients. Format your answer in HTML with cocktail name in <h4> and in all capital letters, ingredients in <ul> with list-style-type: none, under title 'ingredients', and method in <ul> with list-style-type: decimal under title 'method'. Add the type of glassware to serve at the end, under title 'glassware'. Please add <br> when necessary. Don't include any images.";
 
