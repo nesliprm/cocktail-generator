7d83770b588e2a0556c68d3fb0fec71df376f36a
nesliprm <nesliprm@gmail.com>
2024-02-12 18:48:56 +0100

    removed IBA from prompt and moved it to context

--- src/index.js
+++ src/index.js
@@ -12,13 +12,11 @@ function generateCocktail(event) {
 
   let userInput = document.querySelector("#cocktail-input");
   let apiKey = "040oaf210f6td0bbf39be33790d96a71";
-  let prompt = `Display an official IBA cocktail recipe that has ${userInput.value} in it.`;
+  let prompt = `Display a cocktail recipe that has ${userInput.value} in it.`;
   let context =
-    "Please give clear instructions and provide measurements both in oz and ml. List ALL the necessary ingredients. Format your answer in HTML with cocktail name in <h4> and in all capital letters, ingredients in <ul> with list-style-type: none, under title 'ingredients', and method in <ul> with list-style-type: decimal under title 'method'. Add the type of glassware to serve at the end, under title 'glassware'. Please add <br> when necessary. Don't include any images.";
+    "Please find a IBA certified cocktail. Give clear instructions and provide measurements both in oz and ml. List ALL the necessary ingredients. Format your answer in HTML with cocktail name in <h4> and in all capital letters, ingredients in <ul> with list-style-type: none, under title 'ingredients', and method in <ul> with list-style-type: decimal under title 'method'. Add the type of glassware to serve at the end, under title 'glassware'. Please add <br> when necessary. Don't include any images.";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("generating...");
 
   new Typewriter("#cocktail-recipe", {
     strings: [
